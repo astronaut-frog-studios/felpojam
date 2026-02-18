9a3d2998d51f46fc8978b26f6eebd346d0418c48
murilinhops <murilinhorps@gmail.com>
2026-02-18 12:33:45 -0300

    Adding intro scene logic and its sequence

---
+++ source/game/letters-felpojam/scripts/intro/carta.gd
@@ -0,0 +1,15 @@
+extends TextureRect
+
+@onready var button: Button= $Button2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	await get_tree().create_timer(4.0).timeout
+	button.show()
+	
+	
+
+
+func _on_continue_pressed() -> void:
+	print("continue...")
+	#get_tree().change_scene_to_file("res://Scenes/game.tscn")
--- source/game/letters-felpojam/scripts/intro/player_name_input.gd
+++ source/game/letters-felpojam/scripts/intro/player_name_input.gd
@@ -1,9 +1,13 @@
 extends Control
 
+@onready var current: Control = $"."
+@onready var carta: TextureRect = $"../Carta"
 @onready var line_edit: LineEdit = $LineEdit
 @onready var botao: Button = $Button
 
 func _ready() -> void:
+	line_edit.grab_focus()
+	# botao.set_visible(false)
 	botao.disabled = true
 	line_edit.text_changed.connect(_set_button_enabled)
 	line_edit.text_submitted.connect(_on_lineEdit_text_entered)
@@ -15,13 +19,12 @@ func _process(_delta: float) -> void:
 func _on_lineEdit_text_entered(new_text: String) -> void:
 	GlobalName.player_name = new_text
 	botao.disabled = false
-	print(GlobalName.player_name)
 	
 func _set_button_enabled(_val: String) -> void:
 	botao.disabled = false
 
 
 func _on_button_pressed() -> void:
-	GlobalName.player_name = line_edit.text
-	print(GlobalName.player_name)
-	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+	GlobalName.player_name = line_edit.text	
+	current.hide()
+	carta.show()
