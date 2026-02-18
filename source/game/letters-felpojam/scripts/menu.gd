4fe0a3df34a9accf232b79c95307b455310155a5
murilinhops <murilinhorps@gmail.com>
2026-02-17 22:07:10 -0300

    Add script to menu.gd to make it functionable

---
+++ source/game/letters-felpojam/scripts/menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/credits.tscn")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_options_pressed() -> void:
+	print("config...")
+	pass # Replace with function body.
