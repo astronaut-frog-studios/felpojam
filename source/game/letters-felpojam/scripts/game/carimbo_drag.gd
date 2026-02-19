8c252e4a4dff204b63a2bf2a1d0a07caf95412ef
murilinhoPs <murilo.pescatori@nubank.com.br>
2026-02-19 13:00:39 -0300

    Drag-and-drop only duplicates the obj

--- source/game/letters-felpojam/scripts/game/carimbo_drag.gd
+++ source/game/letters-felpojam/scripts/game/carimbo_drag.gd
@@ -3,27 +3,16 @@ extends TextureRect
 # triggers when you click and drag
 func _get_drag_data(_position: Vector2) -> Variant:
 	var preview : Control = Control.new()
-	var preview_texture : TextureRect = TextureRect.new()
-	var preview_texture_2 : TextureRect = TextureRect.new()
-	var preview_obj = Node.new()
+	var preview_obj : Node = Node.new()
 	preview_obj = self.duplicate()
 	preview_obj.remove_child(preview_obj.get_child(1))
-	
-	preview_texture.texture = texture
-	preview_texture.expand_mode = 0
-	preview_texture.position = -Vector2(20, 15) - _position
-	preview_texture_2.texture = get_child(0).texture
-	preview_texture_2.expand_mode = 0
-	preview_texture_2.position = -Vector2(20, 15) - _position
-	
-	#preview.add_child(preview_texture)
+
 	preview.add_child(preview_obj)
 	set_drag_preview(preview)
 	
-	var stamp_texture : Texture2D = get_child(0).texture #armazenar na variavel para retornar
 	self_modulate.a = 0.0
 	get_child(0).visible = false
-	return stamp_texture
+	return get_child(0).texture
 
 # triggers when you hover with a dragged item
 func _can_drop_data(_position: Vector2, data: Variant) -> bool:
