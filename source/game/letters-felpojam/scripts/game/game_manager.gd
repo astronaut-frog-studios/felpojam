1175f9fb2466ad801b8c6f6c3f79a55af30dc1f1
murilinhops <murilinhorps@gmail.com>
2026-02-20 22:36:40 -0300

    Add letter menu art and fixed menu with it Adjust game scene and its nodes sizes

--- source/game/letters-felpojam/scripts/game/game_manager.gd
+++ source/game/letters-felpojam/scripts/game/game_manager.gd
@@ -1,11 +1,24 @@
-extends Node
+class_name GameManager extends Node
 
+enum GameStep { BEGIN, LETTER, FREE_STAMP, POSTMARK, ENDED }  
+enum SeloStep { MELTING_PAINT, DROP_PAINT, POSTMARK_PAINT }
 
-# Called when the node enters the scene tree for the first time.
+@export var current_game_step: GameStep = GameStep.BEGIN
+var current_selo_step: SeloStep = SeloStep.MELTING_PAINT
+
 func _ready() -> void:
+	#current_game_step = GameStep.BEGIN
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
+	match current_game_step:
+		GameStep.BEGIN:
+			print("begin")
+		GameStep.POSTMARK:
+			_postmark_step()
 	pass
+
+func _postmark_step() -> void:
+	current_selo_step = SeloStep.MELTING_PAINT
+	var sinete: TextureRect = get_tree().get_first_node_in_group("sinete") as TextureRect
+	
